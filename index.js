1fe37adf1da54e023450ea8e2f7c36f9ce6a2370
BUSRA KUCUK <busra3590@gmail.com>
2023-06-27 19:51:57 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const userForm = document.querySelector('#user-form');
+userForm.addEventListener('submit', event =>{
+    event.preventDefault();
+    const nameInput = document.querySelector('#name-input');
+    const emailInput =document.querySelector('#email-input');
+    submitData(nameInput.value, emailInput.value);
+});
+function submitData(name, email){
+    const formData = { name: name, email: email };
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML = data["id"]
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message
+      });
+    }
